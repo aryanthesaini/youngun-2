3602f75f515c08296d860d64d9b4597f3d25f3fe
Gunveen Kaur <sainiaryan2222@gmail.com>
2023-06-10 16:45:56 +0530

    icon animation removed

--- components/Client.tsx
+++ components/Client.tsx
@@ -14,14 +14,12 @@ function Client({ directionLeft, name, url }: Props) {
     <div className='group relative flex cursor-pointer'>
       <motion.img
         initial={{
-          x: directionLeft ? -100 : 100,
           opacity: 0,
         }}
         transition={{
           duration: 0.7,
         }}
         whileInView={{
-          x: 0,
           opacity: 1,
         }}
         className='rounded-full border border-gray-500 object-cover w-20 h-20 xl:w-28 xl:h-28 filter group-hover:grayscale transition-duration-300 ease-in-out'
